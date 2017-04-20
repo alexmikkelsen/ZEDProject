0702886542fee7c93e523644f58b826a1c42b584
Agerholm <djohan14@student.aau.dk>
2017-04-20 11:17:07 +0200

    New version
    Changed it to fit with 2.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,168 @@
-/**********************************
-**Using ZED with OpenCV
-**********************************/
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2017, STEREOLABS.
+//
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////
 
-#include <iostream>;
 
-// OpenCV
-#include "opencv2/core.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/videoio.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+/***************************************************************************************************
+** This sample demonstrates how to grab images and depth/disparity map with the ZED SDK          **
+** Both images and depth/disparity map are displayed with OpenCV                                 **
+** Most of the functions of the ZED SDK are linked with a key press event (using OpenCV)         **
+***************************************************************************************************/
 
-// ZED
-#include "zed/Camera.hpp"
-//HAHA. Nej ikke HAHA blah
-// Input from keyboard
-//ALLAN WAS HEERE
-//Daniel was here test
-char keyboard = ' ';
+#include <sl/Camera.hpp>
+#include <opencv2/opencv.hpp>
+#include <sl/Core.hpp>
+#include <sl/defines.hpp>
 
-int main(int argc, char** argv)
-{
-	// Initialize ZED color stream in HD and depth in Performance mode
-	sl::zed::Camera* zed = new sl::zed::Camera(sl::zed::HD1080);
-	sl::zed::InitParams params;
-	params.verbose = true;
-	sl::zed::ERRCODE err = zed->init(params);
+using namespace sl;
 
-	// Quit if an error occurred
-	if (err != sl::zed::SUCCESS) {
-		std::cout << "Unable to init the ZED:" << errcode2str(err) << std::endl;
-		delete zed;
+typedef struct mouseOCVStruct {
+	Mat depth;
+	cv::Size _resize;
+} mouseOCV;
+
+mouseOCV mouseStruct;
+
+static void onMouseCallback(int32_t event, int32_t x, int32_t y, int32_t flag, void * param);
+cv::Mat slMat2cvMat(sl::Mat& input);
+
+int main(int argc, char **argv) {
+
+	// Create a ZED camera object
+	Camera zed;
+
+	// Set configuration parameters
+	InitParameters init_params;
+	init_params.camera_resolution = RESOLUTION_HD720;
+	init_params.depth_mode = DEPTH_MODE_PERFORMANCE;
+	init_params.coordinate_units = sl::UNIT_MILLIMETER;
+	init_params.svo_input_filename = ("C:\GitHub\svo recording\build\mysvo.svo"),false;
+	init_params.camera_fps = 0;
+	init_params.svo_real_time_mode = false;
+	init_params.coordinate_system = COORDINATE_SYSTEM_IMAGE;
+	init_params.sdk_verbose = false;
+	init_params.sdk_gpu_id = -1;
+	init_params.depth_minimum_distance = -1;
+	init_params.camera_disable_self_calib = false;
+	init_params.camera_image_flip = false;
+
+	// Open the camera
+	ERROR_CODE err = zed.open(init_params);
+	if (err != SUCCESS)
 		return 1;
-	}
 
-	// Initialize color image and depth
-	int width = zed->getImageSize().width;
-	int height = zed->getImageSize().height;
-	cv::Mat image(height, width, CV_8UC4, 1);
-	cv::Mat depth(height, width, CV_8UC4, 1);
+	// Set runtime parameters after opening the camera
+	RuntimeParameters runtime_parameters;
+	runtime_parameters.sensing_mode = SENSING_MODE_STANDARD; // Use STANDARD sensing mode
 
-	// Create OpenCV windows
-	cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("Depth", cv::WINDOW_AUTOSIZE);
+															 // Create sl and cv Mat to get ZED left image and depth image
+															 // Best way of sharing sl::Mat and cv::Mat :
+															 // Create a sl::Mat and then construct a cv::Mat using the ptr to sl::Mat data.
+	Resolution image_size = zed.getResolution();
+	sl::Mat image_zed(image_size, sl::MAT_TYPE_8U_C4); // Create a sl::Mat to handle Left image
+	cv::Mat image_ocv = slMat2cvMat(image_zed);
+	sl::Mat depth_image_zed(image_size, MAT_TYPE_8U_C4);
+	cv::Mat depth_image_ocv = slMat2cvMat(depth_image_zed);
 
-	// Settings for windows
+
+	// Create OpenCV images to display (lower resolution to fit the screen)
 	cv::Size displaySize(720, 404);
-	cv::Mat imageDisplay(displaySize, CV_8UC4);
-	cv::Mat depthDisplay(displaySize, CV_8UC4);
+	cv::Mat image_ocv_display(displaySize, CV_8UC4);
+	cv::Mat depth_image_ocv_display(displaySize, CV_8UC4);
+
+	// Mouse callback initialization
+	mouseStruct.depth.alloc(image_size, MAT_TYPE_32F_C1);
+	mouseStruct._resize = displaySize;
+
+	// Give a name to OpenCV Windows
+	cv::namedWindow("Depth", cv::WINDOW_AUTOSIZE);
+	cv::setMouseCallback("Depth", onMouseCallback, (void*)&mouseStruct);
+
+	// Jetson only. Execute the calling thread on 2nd core
+	Camera::sticktoCPUCore(2);
 
 	// Loop until 'q' is pressed
-	while (keyboard != 'q') {
+	char key = ' ';
+	while (key != 'q') {
 
-		// Grab frame and compute depth in FILL sensing mode
-		if (!zed->grab(sl::zed::SENSING_MODE::FILL))
-		{
+		// Grab and display image and depth 
+		if (zed.grab(runtime_parameters) == SUCCESS) {
 
-			// Retrieve left color image
-			sl::zed::Mat left = zed->retrieveImage(sl::zed::SIDE::LEFT);
-			memcpy(image.data, left.data, width*height * 4 * sizeof(uchar));
+			zed.retrieveImage(image_zed, VIEW_RIGHT_UNRECTIFIED_GRAY);
+			zed.retrieveImage(image_zed, VIEW_LEFT); // Retrieve the left image
+			zed.retrieveImage(depth_image_zed, VIEW_DEPTH); //Retrieve the depth view (image)
+			zed.retrieveMeasure(mouseStruct.depth, MEASURE_DEPTH); // Retrieve the depth measure (32bits)
 
-			// Retrieve depth map
-			sl::zed::Mat depthmap = zed->normalizeMeasure(sl::zed::MEASURE::DEPTH);
-			memcpy(depth.data, depthmap.data, width*height * 4 * sizeof(uchar));
+																   // Resize and display with OpenCV
+			cv::resize(image_ocv, image_ocv_display, displaySize);
+			imshow("Image", image_ocv_display);
+			cv::resize(depth_image_ocv, depth_image_ocv_display, displaySize);
+			imshow("Depth", depth_image_ocv_display);
 
-			// Display image in OpenCV window
-			cv::resize(image, imageDisplay, displaySize);
-			cv::imshow("Image", imageDisplay);
-
-			// Display depth map in OpenCV window
-			cv::resize(depth, depthDisplay, displaySize);
-			cv::imshow("Depth", depthDisplay);
+			key = cv::waitKey(10);
 		}
-
-		keyboard = cv::waitKey(30);
-
 	}
 
-	delete zed;
+	zed.close();
+	return 0;
+}
 
+static void onMouseCallback(int32_t event, int32_t x, int32_t y, int32_t flag, void * param) {
+	if (event == CV_EVENT_LBUTTONDOWN) {
+		mouseOCVStruct* data = (mouseOCVStruct*)param;
+		int y_int = (y * data->depth.getHeight() / data->_resize.height);
+		int x_int = (x * data->depth.getWidth() / data->_resize.width);
+
+		sl::float1 dist;
+		data->depth.getValue(x_int, y_int, &dist);
+
+		std::cout << std::endl;
+		if (isValidMeasure(dist))
+			std::cout << "Depth at (" << x_int << "," << y_int << ") : " << dist << "m";
+		else {
+			std::string depth_status;
+			if (dist == TOO_FAR) depth_status = ("Depth is too far.");
+			else if (dist == TOO_CLOSE) depth_status = ("Depth is too close.");
+			else depth_status = ("Depth not available");
+			std::cout << depth_status;
+		}
+		std::cout << std::endl;
+	}
+}
+
+
+cv::Mat slMat2cvMat(sl::Mat& input) {
+	//convert MAT_TYPE to CV_TYPE
+	int cv_type = -1;
+	switch (input.getDataType()) {
+	case sl::MAT_TYPE_32F_C1: cv_type = CV_32FC1; break;
+	case sl::MAT_TYPE_32F_C2: cv_type = CV_32FC2; break;
+	case sl::MAT_TYPE_32F_C3: cv_type = CV_32FC3; break;
+	case sl::MAT_TYPE_32F_C4: cv_type = CV_32FC4; break;
+	case sl::MAT_TYPE_8U_C1: cv_type = CV_8UC1; break;
+	case sl::MAT_TYPE_8U_C2: cv_type = CV_8UC2; break;
+	case sl::MAT_TYPE_8U_C3: cv_type = CV_8UC3; break;
+	case sl::MAT_TYPE_8U_C4: cv_type = CV_8UC4; break;
+	default: break;
+	}
+
+	// cv::Mat data requires a uchar* pointer. Therefore, we get the uchar1 pointer from sl::Mat (getPtr<T>())
+	//cv::Mat and sl::Mat will share the same memory pointer
+	return cv::Mat(input.getHeight(), input.getWidth(), cv_type, input.getPtr<sl::uchar1>(MEM_CPU));
 }
